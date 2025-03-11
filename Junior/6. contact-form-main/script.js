8918dee7b589dc63114e75954b9dae2668ff5047
two8 <iteinfin19@gmail.com>
2025-03-11 16:25:02 +0630

    Add success message and improve form validation interactions

--- Junior/6. contact-form-main/script.js
+++ Junior/6. contact-form-main/script.js
@@ -1,11 +1,44 @@
 const requiredInputs = document.querySelectorAll('.requiredInput'); 
 const button = document.querySelector('button');
+const errorMessages = Array.from(document.querySelectorAll('.error-message'));
+const requiredInputsArray = Array.from(requiredInputs);
+// Reset form on page load/reload
+window.addEventListener('load', () => {
+  const form = document.querySelector('form');
+  form.reset(); // Reset all form inputs
+  
+  // Hide all error messages
+  document.querySelectorAll('.error-message').forEach(message => {
+    message.style.display = 'none';
+  });
+});
 
 requiredInputs.forEach(input => {
+  const errorMessageId = input.getAttribute('aria-describedby');
+  const errorMessage = document.getElementById(errorMessageId);
+  
+  // Add input event listener for text/email inputs and textarea
+  if (input.type !== 'radio' && input.type !== 'checkbox') {
+    input.addEventListener('input', () => {
+      errorMessage.style.display = 'none';
+      input.classList.remove('not-validated');
+    });
+  }
+  
+  // Add change event listener for checkbox and radio
+  if (input.type === 'checkbox' || input.type === 'radio') {
+    input.addEventListener('change', () => {
+      errorMessage.style.display = 'none';
+    });
+  }
+  
   button.addEventListener('click', (e) => {
     e.preventDefault();
     validateInput(input); 
-  }); 
+    if(errorMessages.every(message => message.style.display === 'none') && requiredInputsArray.every(input => input.validity.valid)) {
+      showSuccessMessage();
+    }
+  });  
 }); 
 
 function validateInput(input) {
@@ -27,4 +60,19 @@ function validateInput(input) {
       errorMessage.style.display = 'none';
     }
   }
+
+  if(errorMessage.style.display === 'block') {
+    input.classList.add('not-validated');
+  } else {
+    input.classList.remove('not-validated');
+  }
+}
+
+function showSuccessMessage() {
+  const successMessage = document.querySelector('.success-message');
+  successMessage.classList.add('active-success-message');
+
+  setTimeout(() => {
+    successMessage.classList.remove('active-success-message');
+  }, 3000);
 }
