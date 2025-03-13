8ab442398015417dd7e9b643b5339b5ea355636b
two8 <iteinfin19@gmail.com>
2025-03-13 12:55:31 +0630

    Add responsive navigation menu with mobile support and styling

---
+++ Junior/7. news-homepage-main/script.js
@@ -0,0 +1,16 @@
+window.addEventListener('resize', addActiveClass);
+
+function addActiveClass() {
+  const menuButton = document.querySelector('.menu-button');
+  const navLinks = document.querySelector('.nav-links');
+  
+  if (window.innerWidth > 600) {
+    menuButton.style.display = 'none';
+    navLinks.classList.add('active');
+  } else {
+    menuButton.style.display = 'block';
+    navLinks.classList.remove('active');
+  }
+}
+
+addActiveClass();
