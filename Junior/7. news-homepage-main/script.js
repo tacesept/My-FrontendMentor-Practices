adc24120b49e850fe1961ea219ad070afef1967e
two8 <iteinfin19@gmail.com>
2025-03-15 15:23:55 +0630

    Implement responsive image handling in hero section and update new section article links; remove JavaScript for image switching and enhance CSS with improved padding and hover effects for better user experience.

--- Junior/7. news-homepage-main/script.js
+++
@@ -1,9 +0,0 @@
-const heroImage = document.querySelector(".hero-section img");
-
-window.addEventListener("resize", () => {
-  if (window.innerWidth > 1110) {
-    heroImage.src = "./assets/images/image-web-3-desktop.jpg";
-  } else {
-    heroImage.src = "./assets/images/image-web-3-mobile.jpg";
-  }
-});
