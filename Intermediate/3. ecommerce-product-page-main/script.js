cc5ce1317eab8dd09d93934310d121dfec5ecf96
two8 <iteinfin19@gmail.com>
2025-03-20 10:57:13 +0630

    Implement product image carousel functionality on e-commerce product page; update HTML for navigation buttons, enhance JavaScript for image transitions, and refine SCSS styles for improved layout and responsiveness.

--- Intermediate/3. ecommerce-product-page-main/script.js
+++ Intermediate/3. ecommerce-product-page-main/script.js
@@ -10,6 +10,15 @@ const cartElements = {
   container: document.querySelector(".cart"),
 };
 
+const mainElements = {
+  productImagesList: document.querySelector(".product-images__list"),
+  productImagesListItems: document.querySelectorAll(".product-images__list-item"),
+  previousButton: document.querySelector(".product-images__controls-button.previous"),
+  nextButton: document.querySelector(".product-images__controls-button.next"),
+};
+
+let currentIndex = 0;
+
 const background = document.querySelector(".background");
 
 // Helper Functions
@@ -33,7 +42,18 @@ const toggleCart = () => {
   cartElements.container.classList.toggle("active");
 };
 
+const move = (direction) => {  
+  if (direction === "left" && currentIndex > 0) {
+    currentIndex--;
+  } else if (direction === "right" && currentIndex < mainElements.productImagesListItems.length - 1) {
+    currentIndex++;
+  }
+
+  mainElements.productImagesList.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
 // Event Listeners
 menuElements.button.addEventListener("click", toggleMenu);
 background.addEventListener("click", closeMenu);
 cartElements.button.addEventListener("click", toggleCart);
+mainElements.previousButton.addEventListener("click", () => move("left"));
+mainElements.nextButton.addEventListener("click", () => move("right"));
