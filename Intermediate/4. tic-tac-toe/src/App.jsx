024060032b58ed12ec4d98b3bab6fa676052b444
two8 <iteinfin19@gmail.com>
2025-04-18 18:09:05 +0630

    Refactor Tic-Tac-Toe game structure; update .gitignore to include new dependencies and testing directories, enhance index.html with Google Fonts, and implement new game components for improved functionality. Add prop-types for type checking and reorganize styles in index.scss for better design consistency.

--- Intermediate/4. tic-tac-toe/src/App.jsx
+++ Intermediate/4. tic-tac-toe/src/App.jsx
@@ -1,20 +1,35 @@
-import { useState } from 'react';
-import Menu from './components/Menu';
-import Game from './components/Game';
+import { useState, useEffect } from "react";
+import Menu from "./components/layout/Menu";
+import Game from "./components/layout/Game";
+import "./styles/index.scss";
 
 function App() {
-  const [screen, setScreen] = useState('menu');
+  const [screen, setScreen] = useState("menu");
+  const [gameMode, setGameMode] = useState({
+    playerMark: null,
+    opponent: null,
+  });
 
-  const handleScreen = (screen) => {
-    setScreen(screen);
-  }
+  useEffect(() => {
+    if (gameMode.playerMark && gameMode.opponent) {
+      setScreen("game");
+    }
+  }, [gameMode]);
 
   return (
-    <main className='container'>
-      {screen === 'menu' && <Menu handleScreen={handleScreen} />}
-      {screen === 'game' && <Game handleScreen={handleScreen} />}
+    <main className="container">
+      {screen === "menu" && (
+        <Menu setScreen={setScreen} setGameMode={setGameMode} />
+      )}
+      {screen === "game" && (
+        <Game
+          gameMode={gameMode}
+          setGameMode={setGameMode}
+          setScreen={setScreen}
+        />
+      )}
     </main>
-  )
+  );
 }
 
 export default App;
