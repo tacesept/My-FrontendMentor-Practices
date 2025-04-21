26b2d0e067284b6fcf7c3091795c0e136351048a
two8 <iteinfin19@gmail.com>
2025-04-21 14:45:44 +0630

    Enhance Room Homepage styles and functionality: update index.scss with new animations, responsive menu styles, and improved button hover effects. Modify HeroImg component to include titles and descriptions for images. Refactor SmallScreenHeader to manage menu state and image transitions, enhancing user experience and interactivity.

--- Intermediate/5. room-homepage-master/src/components/HeroImg.jsx
+++ Intermediate/5. room-homepage-master/src/components/HeroImg.jsx
@@ -3,19 +3,25 @@ const HeroImg = [
         id: 1,
         srcM: "./images/mobile-image-hero-1.jpg",
         srcD: "./images/desktop-image-hero-1.jpg",
-        alt: "hero image 1"
+        alt: "hero image 1",
+        title: "Discover innovative ways to decorate",
+        description: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."
     },
     {
         id: 2,
         srcM: "./images/mobile-image-hero-2.jpg",
         srcD: "./images/desktop-image-hero-2.jpg",
-        alt: "hero image 2"
+        alt: "hero image 2",
+        title: "We are available all across the globe",
+        description: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
     },
     {
         id: 3,
         srcM: "./images/mobile-image-hero-3.jpg",
         srcD: "./images/desktop-image-hero-3.jpg",
-        alt: "hero image 3"
+        alt: "hero image 3",
+        title: "Manufactured with the best materials",
+        description: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
     }
 ]
 
--- Intermediate/5. room-homepage-master/src/components/SmallScreenHeader.jsx
+++ Intermediate/5. room-homepage-master/src/components/SmallScreenHeader.jsx
@@ -1,29 +1,116 @@
 import HeroImg from "./HeroImg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function SmallScreenHeader() {
   const [currentImg, setCurrentImg] = useState(0);
+  const [translate, setTranslate] = useState(0);
+  const [animate, setAnimate] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const navText = ["home", "shop", "about", "contact"];
 
-  const handleImgChange = (index) => {
-    if (index < 0) {
-      index = 0;
-    } else if (index >= HeroImg.length) {
-      index = HeroImg.length - 1;
+  useEffect(() => {
+    const closeMenu = () => setMenuOpen(false);
+
+    if (menuOpen) {
+      window.addEventListener("scroll", closeMenu, { passive: true });
+      window.addEventListener("resize", closeMenu, { passive: true });
+      window.addEventListener("wheel", closeMenu, { passive: true });
+      window.addEventListener("touchmove", closeMenu, { passive: true });
     }
 
-    setCurrentImg(index);
+    return () => {
+      window.removeEventListener("scroll", closeMenu);
+      window.removeEventListener("resize", closeMenu);
+      window.removeEventListener("wheel", closeMenu);
+      window.removeEventListener("touchmove", closeMenu);
+    };
+  }, [menuOpen]);
+
+  const handleImgChange = (direction) => {
+    if (direction === "right" && currentImg < HeroImg.length - 1) {
+      if (animate) return;
+      setCurrentImg(currentImg + 1);
+      setTranslate(translate - 100);
+
+      setAnimate(true);
+
+      setTimeout(() => {
+        setAnimate(false);
+      }, 1000);
+    } else if (direction === "left" && currentImg > 0) {
+      if (animate) return;
+      setCurrentImg(currentImg - 1);
+      setTranslate(translate + 100);
+      setAnimate(true);
+
+      setTimeout(() => {
+        setAnimate(false);
+      }, 1000);
+    }
+  };
+
+  const handleMenuClick = (condition) => {
+    if (condition) {
+      setMenuOpen(!menuOpen);
+    } else {
+      setMenuOpen(false);
+    }
+  };
+
+  const handleOverlayClick = () => {
+    setMenuOpen(false);
   };
 
   return (
     <header className="sm-screen__header">
+      <div
+        className={`overlay ${menuOpen ? "active" : ""}`}
+        onClick={handleOverlayClick}
+      ></div>
+      <div
+        id="menu"
+        className={`sm-screen__menu-popup ${menuOpen ? "active" : ""}`}
+      >
+        <button aria-label="close menu" onClick={() => handleMenuClick(true)}>
+          <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M14.364.222l1.414 1.414L9.414 8l6.364 6.364-1.414 1.414L8 9.414l-6.364 6.364-1.414-1.414L6.586 8 .222 1.636 1.636.222 8 6.586 14.364.222z"
+              fill="#000"
+              fillRule="evenodd"
+              opacity=".201"
+            />
+          </svg>
+        </button>
+        <nav>
+          <ul>
+            {navText.map((text, index) => (
+              <li key={index}>
+                <a href="#">{text}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+
       <div className="sm-screen__top">
-        <img
-          className="img-hero"
-          src={HeroImg[currentImg].srcM}
-          alt={HeroImg[currentImg].alt}
-        />
+        <ul style={{ marginInline: `${translate}%` }}>
+          {HeroImg.map((img, index) => (
+            <li key={index}>
+              <img
+                src={img.srcM}
+                alt={img.alt}
+                className={index === currentImg ? "active" : ""}
+              />
+            </li>
+          ))}
+        </ul>
         <div className="sm-screen__top-header">
-          <button>
+          <button
+            aria-expanded={menuOpen}
+            aria-controls="menu"
+            aria-label="open menu"
+            onClick={() => handleMenuClick(true)}
+          >
             <img src="./images/icon-hamburger.svg" alt="hamburger menu" />
           </button>
           <a href="#">
@@ -33,45 +120,55 @@ export default function SmallScreenHeader() {
           <div></div>
         </div>
         <div className="sm-screen__top-buttons">
-          <button onClick={() => handleImgChange(currentImg - 1)}>
-            <svg className="arrow-left" width="14" height="24" xmlns="http://www.w3.org/2000/svg">
+          <button
+            aria-label="previous image"
+            onClick={() => handleImgChange("left")}
+          >
+            <svg
+              className="arrow-left"
+              width="14"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path
                 d="M13 0L1 12l12 12"
                 stroke="#FFF"
                 fill="none"
-                fill-rule="evenodd"
+                fillRule="evenodd"
               />
             </svg>
           </button>
-          <button onClick={() => handleImgChange(currentImg + 1)}>
-            <svg className="arrow-right" width="14" height="24" xmlns="http://www.w3.org/2000/svg">
+          <button
+            aria-label="next image"
+            onClick={() => handleImgChange("right")}
+          >
+            <svg
+              className="arrow-right"
+              width="14"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path
                 d="M1 0l12 12L1 24"
                 stroke="#FFF"
                 fill="none"
-                fill-rule="evenodd"
+                fillRule="evenodd"
               />
             </svg>
           </button>
         </div>
       </div>
 
-      <div className="sm-screen__bottom">
-        <h1>Discover innovative ways to decorate</h1>
-        <p>
-          We provide unmatched quality, comfort, and style for property owners
-          across the country. Our experts combine form and function in bringing
-          your vision to life. Create a room in your own style with our
-          collection and make your property a reflection of you and what you
-          love.
-        </p>
+      <div className={`sm-screen__bottom ${animate ? "animate" : ""}`}>
+        <h1>{HeroImg[currentImg].title}</h1>
+        <p>{HeroImg[currentImg].description}</p>
         <button>
           <span>Shop now</span>
           <svg width="40" height="12" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z"
               fill="#000"
-              fill-rule="nonzero"
+              fillRule="nonzero"
             />
           </svg>
         </button>
